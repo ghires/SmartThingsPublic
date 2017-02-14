3e0975c714d9c3b239327903d18b0f736a60cc38
Gary Hires <gary@hiresfamily.com>
2017-02-14 16:58:07 -0600

    commiting to personal repo

---
+++ smartapps/dcoffing/3-speed-ceiling-fan-thermostat.src/3-speed-ceiling-fan-thermostat.groovy
@@ -0,0 +1,189 @@
+/**
+ * Virtual Thermostat for 3 Speed Ceiling Fan Control 
+ *  This smartapp provides automatic control of Low, Medium, High speeds of smart fan control device using 
+ *  any temperature sensor. 
+ *  It works best with @ChadCK custom device handler Z-Wave Smart Fan Control located here
+ *  https://community.smartthings.com/t/z-wave-smart-fan-control-custom-device-type/25558
+ *  along with the GE 12730 Z-Wave Smart Fan Control hardware. This smartapp was modified from the SmartThings
+ *  Virtual Thermostat code which only allowed for on/off control of a switch. 
+ *  Thanks to @krlaframboise for his patient help and knowledge in solving poor coding by a first time coder.
+ *
+ *  Copyright 2016 Dale Coffing
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *
+ *  Author: Dale Coffing
+ *  Version: 0.9e ^^^
+ *
+ * Change Log
+ * 2016-6-6  minor changes to text, labels, for clarity, (^^^e)default to NO-Manual for thermostat mode 
+ * 2016-5-5c clean code, added current ver section header, allow for multiple fan controllers,
+ *           replace icons to ceiling fan, modify name from Control to Thermostat
+ * 2016-5-5b @krlaframboise change to bypasses the temperatureHandler method and calls the evaluate method
+ *           with the current temperature and setpoint setting
+ * 2016-5-5  autoMode added for manual override of auto control
+ * 2016-5-4b cleaned debug logs, removed heat-cool selection, removed multiple stages
+ * 2016-5-3  fixed error on not shutting down, huge shout out to my bro Stephen Coffing in the logic formation 
+ * 
+ * Consider/Feature Requests:
+ *	-Is emergency setpoint function necessary
+ *	-User adjustable temperature differentials for Low, Med, High
+ *	-Global Mode check and/or virtual switch to disable app 
+ *
+ */
+definition(
+    name: "3 Speed Ceiling Fan Thermostat",
+    namespace: "dcoffing",
+    author: "Dale Coffing",
+    description: "Control a 3 Speed Ceiling Fan using Low, Medium, High speeds with any temperature sensor.",
+    category: "My Apps",
+    iconUrl: "http://cdn.device-icons.smartthings.com/Lighting/light24-icn.png",
+    iconX2Url: "http://cdn.device-icons.smartthings.com/Lighting/light24-icn@2x.png"
+)
+
+preferences {
+	section("Select a temperature sensor to control the fan..."){
+		input "sensor", "capability.temperatureMeasurement",
+        	multiple:false, title: "Temperature Sensor", required: true 
+	}
+    section("Select the fan control hardware(s)..."){
+		input "fanDimmer", "capability.switchLevel", 
+	    	multiple: true, title: "Fan Control device(s)", required: true
+	}
+	section("Enter the desired room temperature (ie 72.5)..."){
+		input "setpoint", "decimal", title: "Room Setpoint Temp"
+	}
+	section("When there's been movement from (optional, leave blank to not require motion)..."){
+		input "motion", "capability.motionSensor", title: "Select Motion device", required: false
+	}
+	section("Within this number of minutes..."){
+		input "minutes", "number", title: "Minutes", required: false
+	}
+	section("But run Ceiling Fan above this high limit temperature with or without motion..."){
+		input "emergencySetpoint", "decimal", title: "High Limit Setpoint Temp", required: false
+	}
+	section("Select Thermostat mode desired (defaulted 'Manual')..."){
+		input "autoMode", "enum", title: "Enable Ceiling Fan Thermostat?", options: ["NO-Manual","YES-Auto"], required: true
+	}
+    section ("3 Speed Ceiling Fan Thermostat - Ver 0.9e^^^") { }
+}
+def installed(){
+	subscribe(sensor, "temperature", temperatureHandler)
+	if (motion) {
+		subscribe(motion, "motion", motionHandler)
+	}
+}
+
+def updated(){
+	unsubscribe()
+	subscribe(sensor, "temperature", temperatureHandler)
+	if (motion) {
+		subscribe(motion, "motion", motionHandler)
+	}					// @krlaframboise fix for setpoint changes to immediately act.
+    handleTemperature(sensor.currentTemperature) //The change I recommended bypasses the temperatureHandler method 											
+} 						 //and calls the evaluate method with the current temperature and
+						//setpoint setting. If you want to execute the same code that the                                                
+def temperatureHandler(evt){			//temperatureHandler method calls, you should break that code into					
+    handleTemperature(evt.doubleValue)		 //a separate method.
+}						//
+def handleTemperature(temp) {			//
+	def isActive = hasBeenRecentMotion()
+	if (isActive || emergencySetpoint) {
+		evaluate(temp, isActive ? setpoint : emergencySetpoint)
+	}
+	else {
+     	fanDimmer.off()
+        fanDimmer.setLevel(0)
+	}
+}
+
+def motionHandler(evt){
+	if (evt.value == "active") {
+		def lastTemp = sensor.currentTemperature
+		if (lastTemp != null) {
+			evaluate(lastTemp, setpoint)
+		}
+	} else if (evt.value == "inactive") {
+		def isActive = hasBeenRecentMotion()
+		log.debug "INACTIVE($isActive)"
+		if (isActive || emergencySetpoint) {
+			def lastTemp = sensor.currentTemperature
+			if (lastTemp != null) {
+				evaluate(lastTemp, isActive ? setpoint : emergencySetpoint)
+			}
+		}
+		else {
+     	    fanDimmer.off()
+            fanDimmer.setLevel(0)
+		}
+	}
+}
+
+private evaluate(currentTemp, desiredTemp)
+{
+log.debug "EVALUATE($currentTemp, $desiredTemp, $fanDimmer.currentSwitch, $fanDimmer.currentLevel, $autoMode)"
+   // these are temp differentials desired from setpoint for Low, Medium, High fan speeds
+    def LowDiff = 1.0 
+    def MedDiff = 2.0
+    def HighDiff = 3.0
+	if (autoMode == "YES-Auto") {
+    	if (currentTemp - desiredTemp >= HighDiff) {
+        	// turn on fan high speed
+       		fanDimmer.setLevel(90) 
+            log.debug "HIGH speed($currentTemp, $desiredTemp, $fanDimmer.currentLevel)"
+      		}
+        	    else if  (currentTemp - desiredTemp >= MedDiff) {
+            	    // turn on fan medium speed
+            	    fanDimmer.setLevel(60)
+                    log.debug "MED speed($currentTemp, $desiredTemp, $fanDimmer.currentLevel)"
+       		    }
+            		 else if  (currentTemp - desiredTemp >= LowDiff) {
+              		    // turn on fan low speed
+               			if (fanDimmer.currentSwitch == "off") { // if fan is OFF protect motor by  
+               	  		   fanDimmer.setLevel(90)                	// starting fan in High speed temporarily then 
+                  		   fanDimmer.setLevel(30, [delay: 3000]) 	// change to Low speed after 3 seconds
+                           log.debug "LOW speed after HI3secs($currentTemp, $desiredTemp, $fanDimmer.currentLevel)"
+          			}
+               			else {
+                  		   fanDimmer.setLevel(30) 	//fan is already running, not necessary to protect motor
+               			   }			           //set Low speed immediately
+                           log.debug "LOW speed immediately($currentTemp, $desiredTemp, $fanDimmer.currentLevel)"
+
+   		}
+		else if (desiredTemp - currentTemp >= LowDiff) {   //below setpoint, turn off fan, zero level
+            fanDimmer.setLevel(0) // this is not executing a Level change to zero?
+            fanDimmer.off()
+            log.debug "below SP+Diff ($currentTemp, $desiredTemp, $fanDimmer.currentLevel)"
+		}
+	else {       //bypassing automatic control due to autoMode in OFF
+         log.debug "autoMode YES-MANUAL? else OFF($currentTemp, $desiredTemp,$fanDimmer.currentLevel, $autoMode)"
+		 }       //and fan remains at last commanded setLevel
+	}
+  log.debug "If autoMode NO-Manual($currentTemp, $desiredTemp, $fanDimmer.currentLevel)"
+}
+private hasBeenRecentMotion()
+{
+	def isActive = false
+	if (motion && minutes) {
+		def deltaMinutes = minutes as Long
+		if (deltaMinutes) {
+			def motionEvents = motion.eventsSince(new Date(now() - (60000 * deltaMinutes)))
+			log.trace "Found ${motionEvents?.size() ?: 0} events in the last $deltaMinutes minutes"
+			if (motionEvents.find { it.value == "active" }) {
+				isActive = true
+			}
+		}
+	}
+	else {
+		isActive = true
+	}
+	isActive
+}
---
+++ smartapps/michaelstruck/alexa-helper.src/alexa-helper.groovy
@@ -0,0 +1,274 @@
+/**
+ *  Alexa Helper-Parent
+ *
+ *  Copyright 2015 Michael Struck
+ *  Version 3.2.0 12/5/15
+ * 
+ *  Version 1.0.0 - Initial release
+ *  Version 2.0.0 - Added 6 slots to allow for one app to control multiple on/off actions
+ *  Version 2.0.1 - Changed syntax to reflect SmartThings Routines (instead of Hello, Home Phrases)
+ *  Version 2.1.0 - Added timers to the first 4 slots to allow for delayed triggering of routines or modes
+ *  Version 2.2.1 - Allow for on/off control of switches and changed the UI slightly to allow for other controls in the future
+ *  Version 2.2.2 - Fixed an issue with slot 4
+ *  Version 3.0.0 - Allow for parent/child 'slots'
+ *  Version 3.1.0 - Added ability to control a thermostat
+ *  Version 3.1.1 - Refined thermostat controls and GUI (thanks to @SDBOBRESCU "Bobby")
+ *  Version 3.2.0 - Added ability to a connected speaker
+ * 
+ *  Uses code from Lighting Director by Tim Slagle & Michael Struck
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
+
+definition(
+    name: "Alexa Helper",
+    singleInstance: true,
+    namespace: "MichaelStruck",
+    author: "Michael Struck",
+    description: "Allows for routines or modes to be tied to various switch's state controlled by Alexa (Amazon Echo).",
+    category: "My Apps",
+    iconUrl: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa.png",
+    iconX2Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/MichaelStruck/SmartThings/master/Other-SmartApps/AlexaHelper/Alexa@2x.png")
+
+preferences {
+    page name:"mainPage"
+}
+
+//Show main page
+def mainPage() {
+    dynamicPage(name: "mainPage", title: "Alexa Helper Scenarios", install: true, uninstall: true) {
+            section {
+            	app(name: "childScenarios", appName: "Alexa Helper-Scenario", namespace: "MichaelStruck", title: "Create New Alexa Scenario...", multiple: true)
+            }
+            section ("Thermostat") {
+            	href "tstatControl", title: "Thermostat Controls", description: getDescTstat(), state: greyOut(vDimmerTstat, tstat)
+            }
+            section ("Speaker") {
+            	href "speakerControl", title: "Speaker Controls", description: getDescSpeaker(), state: greyOut(vDimmerSpeaker, speaker)
+            }
+            section([title:"Options", mobileOnly:true]) {
+            	label title:"Assign a name", required:false
+            	href "pageAbout", title: "About ${textAppName()}", description: "Tap to get application version, license and instructions"
+        	}
+    }
+}
+
+page(name: "tstatControl", title: "Thermostat Controls"){
+   	section {
+    	input "vDimmerTstat", "capability.switchLevel", title: "Alexa Dimmer Switch", multiple: false, required:false
+		input "tstat", "capability.thermostat", title: "Thermostat To Control", multiple: false , required: false
+    	input "upLimitTstat", "number", title: "Thermostat Upper Limit", required: false
+    	input "lowLimitTstat", "number", title: "Thermostat Lower Limit", required: false
+        input "autoControlTstat", "bool", title: "Control when thermostat in 'Auto' mode", defaultValue: false
+	}
+}
+
+page (name: "speakerControl", title: "Speaker Controls"){
+   	section {
+    	input "vDimmerSpeaker", "capability.switchLevel", title: "Alexa Dimmer Switch", multiple: false, required:false
+		input "speaker", "capability.musicPlayer", title: "Connected Speaker To Control", multiple: false , required: false
+    	input "speakerInitial", "number", title: "Volume when speaker turned on", required: false
+        input "upLimitSpeaker", "number", title: "Volume Upper Limit", required: false
+    	input "lowLimitSpeaker", "number", title: "Volume  Lower Limit", required: false
+       	input "nextSwitch", "capability.switch", title: "Next Track Switch", multiple: false, required: false
+       	input "prevSwitch", "capability.switch", title: "Previous Track Switch", multiple: false, required: false
+    }
+}
+
+page(name: "pageAbout", title: "About ${textAppName()}") {
+	section {
+    	paragraph "${textVersion()}\n${textCopyright()}\n\n${textLicense()}\n"
+    }
+    section("Instructions") {
+        paragraph textHelp()
+    }
+}
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    childApps.each {child ->
+            log.info "Installed Scenario: ${child.label}"
+    }
+	if (vDimmerTstat && tstat) {
+    	subscribe (vDimmerTstat, "level", "thermoHandler")
+	}
+    if (vDimmerSpeaker && speaker) {
+    	subscribe (vDimmerSpeaker, "level", "speakerVolHandler")
+        subscribe (vDimmerSpeaker, "switch", "speakerOnHandler")
+        if (nextSwitch) {
+        	subscribe (nextSwitch, "switch", "controlNextHandler")
+        }
+        if (prevSwitch) {
+        	subscribe (prevSwitch, "switch", "controlPrevHandler")
+        } 
+	}
+}
+
+//Thermostat Handler
+def thermoHandler(evt){
+    // Get settings between limits
+    def tstatLevel = vDimmerTstat.currentValue("level")
+    if (upLimitTstat && vDimmerTstat.currentValue("level") > upLimitTstat){
+    	tstatLevel = upLimitTstat
+    }
+    if (lowLimitTstat && vDimmerTstat.currentValue("level") < lowLimitTstat){
+    	tstatLevel = lowLimitTstat
+    }
+	//Turn thermostat to proper level depending on mode
+    def tstatMode=tstat.currentValue("thermostatMode")
+    if (tstatMode == "heat") {
+        tstat.setHeatingSetpoint(tstatLevel)	
+    }
+    if (tstatMode == "cool") {
+        tstat.setCoolingSetpoint(tstatLevel)	
+    }
+    if (tstatMode == "auto" && autoControlTstat){
+    	tstat.setHeatingSetpoint(tstatLevel)
+        tstat.setCoolingSetpoint(tstatLevel)
+    }
+    log.debug "Thermostat set to ${tstatLevel}"
+}
+
+//Speaker Volume Handler
+def speakerVolHandler(evt){
+    def speakerLevel = vDimmerSpeaker.currentValue("level")
+    if (speakerLevel == 0) {
+    	vDimmerSpeaker.off()	
+    }
+    else {
+        // Get settings between limits
+        if (upLimitSpeaker && vDimmerSpeaker.currentValue("level") > upLimitSpeaker){
+    		speakerLevel = upLimitSpeaker
+    	}
+    	if (lowLimitSpeaker && vDimmerSpeaker.currentValue("level") < lowLimitSpeaker){
+    		speakerLevel = lowLimitSpeaker
+    	}
+		//Turn speaker to proper volume
+    	speaker.setLevel(speakerLevel)
+	}
+}
+
+def speakerOnHandler(evt){
+	if (evt.value == "on"){
+    	if (speakerInitial){
+        	def speakerLevel = speakerInitial
+    		vDimmerSpeaker.setLevel (speakerInitial)
+        }
+    	speaker.play()
+    }
+    else {
+    	speaker.stop()
+    }
+}
+
+def controlNextHandler(evt){
+    speaker.nextTrack()
+}
+
+def controlPrevHandler(evt){
+	speaker.previousTrack()
+}
+
+//Common Methods
+
+def getDescTstat(){
+    def result = "Tap to setup theromstat controls"
+    if (vDimmerTstat && tstat) {
+		result = "${vDimmerTstat} controls ${tstat}"
+        if (upLimitTstat) {
+           	result += "\nLimits: Not greater than ${upLimitTstat}"
+		}
+		if (lowLimitTstat) {
+        	result += " and no lower than ${lowLimitTstat}"
+        }
+        if (autoControlTstat){
+        	result += "\nControl when thermostat in 'Auto' mode"
+        }    
+	}
+    result
+}
+
+def getDescSpeaker(){
+    def result = "Tap to setup speaker controls"
+    if (vDimmerSpeaker && speaker) {
+		result = "${vDimmerSpeaker} controls ${speaker}"
+        if (upLimitSpeaker) {
+           	result += "\nVolume Limits: Not greater than ${upLimitSpeaker}"
+		}
+		if (lowLimitSpeaker) {
+        	result += " and no lower than ${lowLimitSpeaker}"
+        }
+        if (nextSwitch){
+        	result += "\nNext Track Switch: ${nextSwitch}"
+        }
+        if (prevSwitch){
+        	result += "\nPrevious Track Switch: ${prevSwitch}"
+        }
+	}
+    result
+}
+
+def greyOut(param1,param2){
+    def result = param1 && param2 ? "complete" : ""
+}
+
+//Version/Copyright/Information/Help
+
+private def textAppName() {
+	def text = "Alexa Helper"
+}	
+
+private def textVersion() {
+    def text = "Version 3.2.0 (12/05/2015)"
+}
+
+private def textCopyright() {
+    def text = "Copyright © 2015 Michael Struck"
+}
+
+private def textLicense() {
+    def text =
+		"Licensed under the Apache License, Version 2.0 (the 'License'); "+
+		"you may not use this file except in compliance with the License. "+
+		"You may obtain a copy of the License at"+
+		"\n\n"+
+		"    http://www.apache.org/licenses/LICENSE-2.0"+
+		"\n\n"+
+		"Unless required by applicable law or agreed to in writing, software "+
+		"distributed under the License is distributed on an 'AS IS' BASIS, "+
+		"WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. "+
+		"See the License for the specific language governing permissions and "+
+		"limitations under the License."
+}
+
+private def textHelp() {
+	def text =
+		"Ties SmartThings routines, modes or switches to the on/off state of a specifc switch. You may also control a thermostat using a dimmer control. "+
+		"Perfect for use with Alexa.\n\nTo use, first create the required momentary button tiles or virtual switches/dimmers from the SmartThings IDE. "+
+		"You may also use any physical switches already associated with SmartThings. Include these switches within the Echo/SmartThings app, then discover the switches on the Echo. "+
+		"For on/off or momentary buttons, add a scenario and choose the discovered switch to be monitored and tie the on/off state of that switch to a specific routine, mode or on/off state of other switches. "+
+		"The routine, mode or switches will fire with the switch state change, except in cases where you have a delay specified. This time delay is optional. "+
+		"\n\nPlease note that if you are using a momentary switch you should only define the 'on' action within each scenario.\n\n" +
+		"To control a thermostat, tap the thermostat controls and choose a dimmer switch (usually a virtual dimmer) and the thermostat you wish to control. "+
+		"You can also limit the range the thermostat will reach (for example, even if you accidently set the dimmer to 100, the value sent to the "+
+		"thermostat could be limited to 72)."+
+        	"\nTo control a connected speaker, tap the speaker controls and choose a dimmer switch (usually a virtual dimmer) and speaker you wish to control. "+
+		"You can set the initial volume upon turning on the speaker, along with volume limites. Finally. you can utilize other virtual switches to choose next/previous tracks."
+}
